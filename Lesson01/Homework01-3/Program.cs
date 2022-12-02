cf58a0de2b56a4ec71f9bbcc35e03029829a73be
SergeyMalkin <10.09.89@inbox.ru>
2022-12-02 11:56:48 +0300

    first commit

---
+++ Lesson01/Homework01-3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введи число: ");
+
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+
