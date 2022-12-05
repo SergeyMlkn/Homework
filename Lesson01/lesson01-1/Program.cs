43cf6bee4f8509a6c2c28a417a098e4ef906eeb3
SergeyMalkin <10.09.89@inbox.ru>
2022-12-05 15:30:58 +0300

    первое сохранение

---
+++ Lesson01/lesson01-1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Работа с текстом
+// Дан текст. В нем нужно все пробелы заменить четочками
+// маленькие "к" заменить большими "К"
+// большые "С" заменить маленькики "с"
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступум согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+// s [3] // позиция в регистре (начинается с 0)
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
