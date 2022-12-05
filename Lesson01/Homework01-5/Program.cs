4fbfcbe3bb6bf1da2cd6135dd774d3b5661eede9
SergeyMalkin <10.09.89@inbox.ru>
2022-12-05 11:29:02 +0300

    end

--- Lesson01/Homework01-5/Program.cs
+++ Lesson01/Homework01-5/Program.cs
@@ -5,4 +5,19 @@
 // 3. Составить новое число
 // 4. Показать его на экране
 
-Console.WriteLine("Hello, World!");
+Console.Clear(); // чистка терминала от прежних записей
+
+Console.WriteLine("start");
+
+int value = new Random().Next(100, 1000); // генерация случайного числа с 100 по 999
+
+int a = value / 100;
+int b = (value / 10) % 10;
+int c = value % 10;
+
+int result = a * 10 + c;
+
+Console.WriteLine(value);
+Console.WriteLine(result);
+
+Console.WriteLine("end");
