6fad8680329c23af793fd4f69ca6aacdc578cb67
SergeyMalkin <10.09.89@inbox.ru>
2022-12-06 11:47:05 +0300

    end

---
+++ Lesson01/Homework01-9/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите методы, который задаёт массив из 8 элементов и выводит их на экран.
+
+int num = 8;
+
+void FillArray(int[] array)
+{
+    int i=0;
+    int count = array.Length;
+    while (i < array.Length)
+    {
+        array[i] = new Random().Next(1, num);
+        i++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    int count = array.Length;
+    while (i < array.Length)
+    {
+        Console.Write(array[i]);
+        i++;
+    }
+}
+int[] array = new int [num];
+FillArray(array);
+PrintArray(array);
