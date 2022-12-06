f019766bccae28df2cd4fba762daf6c2bb20f20f
SergeyMalkin <10.09.89@inbox.ru>
2022-12-06 11:38:09 +0300

    end

---
+++ Lesson01/Homework01-8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите свое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int sum = 0;
+while (num != 0)
+{
+    i = num % 10;
+    sum = sum + i;
+    num = num / 10;
+}
+Console.WriteLine(sum);
