b52d720fa94e9dd20e470bfa37a1293a6bdb9350
SergeyMalkin <10.09.89@inbox.ru>
2022-12-06 11:32:34 +0300

    end

---
+++ Lesson01/Homework01-7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Math.Pow использовать нельзя
+// 3, 5 -> 243 (3⁵)
+
+int Method(int num, int y)
+{
+    int i;
+    int result = 1;
+    for (i = 1; i <= y; i++)
+    {
+        result = result * num;
+    }
+    return result;
+}
+int res = Method(3, 5);
+Console.WriteLine(res);
